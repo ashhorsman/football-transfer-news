f470a56f08fcf38539a9a8c4be0aa0ad53c6f5b8
Ash <32211126+ashhorsman@users.noreply.github.com>
2024-07-16 14:25:29 +0100

    Start again

--- backend/backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from transfers.views import get_transfer_news
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/news/', get_transfer_news),
-]
--- backend/transfers/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import TransferNews
-
-class TransferNewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TransferNews
-        fields = '__all__'
